c727843a3d4bc569e94808e4d8e41e581cf55d1a
Annia Forney <anniaforney@gmail.com>
2019-05-23 06:19:02 +0000

    Add to Read me

---
+++ scraper.rb
@@ -0,0 +1,12 @@
+class Scraper 
+  
+  def run 
+    puts "Hello customer" 
+    puts "Here are a list of healing crystals and their meanings"
+    puts "      "
+    puts "-----------"
+    puts "       "
+    html = open("https://www.feelcrystals.com.au/crystal-meanings/")
+    doc = Nokogiri::HTML(html)
+  end 
+end 
